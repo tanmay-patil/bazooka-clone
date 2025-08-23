9743eba4a9a1bfd7ccff68822ab3849fb61c469c
Pranay Dutta <pranaydutta89@gmail.com>
2025-08-23 19:46:52 +0530

    fix: move Clarity initialization inside production check for better environment handling

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,11 @@ import './styles/minimal.css';
 import App from './App';
 import Clarity from '@microsoft/clarity';
 
-const projectId = "szd1owl9nb"
+if (process.env.NODE_ENV === "production") {
+  const projectId = "szd1owl9nb"
 
-Clarity.init(projectId);
+  Clarity.init(projectId);
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

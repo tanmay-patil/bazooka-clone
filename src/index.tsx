a0c194dde5be46a0f8a5facbd4246225bdbff3e4
Pranay Dutta <pranaydutta89@gmail.com>
2025-08-23 19:40:18 +0530

    feat: integrate Microsoft Clarity for enhanced user analytics and update package dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/minimal.css';
 import App from './App';
+import Clarity from '@microsoft/clarity';
+
+const projectId = "szd1owl9nb"
+
+Clarity.init(projectId);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
